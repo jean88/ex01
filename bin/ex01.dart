09a8ffac7d2d3f47d9a252e7ac88b30f4b54417c
jean88 <jean-francois.robichaud.1@ulaval.ca>
2013-09-07 21:11:18 -0400

    Exercice 1

---
+++ bin/ex01.dart
@@ -0,0 +1,18 @@
+int nombre =1;
+
+void main() {
+
+   
+  while(nombre<=100 ){
+   
+   if(nombre%3 == 0){print("fizz");}
+   if(nombre%5 ==0){print("buzz");}
+   
+   else
+   {print("$nombre");}
+   nombre++;
+   }
+   
+  }
+
+
