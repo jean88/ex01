c565afc863e8ed0915a2349fa7268f1cf5502cb3
Dzenan Ridjanovic <dzenanr@gmail.com>
2013-09-11 18:10:11 -0400

    add doc; format code

--- bin/ex01.dart
+++ bin/ex01.dart
@@ -1,18 +1,12 @@
 int nombre =1;
 
 void main() {
-
-   
-  while(nombre<=100 ){
-   
-   if(nombre%3 == 0){print("fizz");}
-   if(nombre%5 ==0){print("buzz");}
-   
-   else
-   {print("$nombre");}
-   nombre++;
-   }
-   
+  while (nombre <= 100 ) {
+    if (nombre % 3 == 0) {print("fizz");}
+    else if (nombre % 5 == 0) {print("buzz");}
+    else {print("$nombre");}
+    nombre++;
   }
+}
 
 
