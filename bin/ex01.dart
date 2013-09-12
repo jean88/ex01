adad90cd91e94ad8e28c2c412b39728eda9c9c2e
jean88 <jean-francois.robichaud.1@ulaval.ca>
2013-09-12 17:56:43 -0400

    modification

--- bin/ex01.dart
+++ bin/ex01.dart
@@ -4,9 +4,9 @@ void main() {
 
    
   while(nombre<=100 ){
-   
-   if(nombre%3 == 0){print("fizz");}
-   if(nombre%5 ==0){print("buzz");}
+    if(nombre%3 == 0 && nombre%5 ==0){print("$nombre : fizz buzz");}
+    else if(nombre%3 == 0){print("$nombre : fizz");}
+    else if(nombre%5 ==0){print("$nombre : buzz");}
    
    else
    {print("$nombre");}
